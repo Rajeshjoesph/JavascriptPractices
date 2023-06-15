23e40a917fe73aefeed2de5dbd76828b2796d8e0
“rajeshjoshph” <rajeshkannan1999.rk@gmail.com>
2023-06-15 09:45:31 -0700

    ADDED: insert json object with do while function

--- index.js
+++ index.js
@@ -2,27 +2,52 @@ const prompt=require('prompt-sync')();
 const fs=require('fs');
 writeData=require("./inventory");
 
-let Pcode=prompt("Enter PCode:");
-let Desc=prompt("Enter Descripation:");
-let Category=prompt("Enter Category:");
-let Mrp=prompt("Enter MRP:");
-let Upc=prompt("Enter Upc:");
 
-var json={
-    "Pcode":Pcode,
-    "Desc":Desc,
-    "Category":Category,
-    "Mrp":Mrp,
-    "Upc":Upc
-};
 
-writeData.push(json);
 
-let obj=JSON.stringify(writeData);
 
-fs.writeFileSync("inventory.json",obj);
+// writeData.push(inventory);
 
-let output=fs.readFileSync('inventory.json');
+// let obj=JSON.stringify(writeData);
 
-let data=JSON.parse(output);
-console.log(data);
+// fs.writeFileSync("inventory.json",obj);
+
+
+
+// var input=prompt("IF you want enter data, Press 1 Then  Exist Press 0: ");
+
+
+do{
+    let Pcode=prompt("Enter PCode:");
+    let Desc=prompt("Enter Descripation:");
+    let Category=prompt("Enter Category:");
+    let Mrp=prompt("Enter MRP:");
+    let Upc=prompt("Enter Upc:");
+
+    var inventory={
+        "Pcode":Pcode,
+        "Desc":Desc,
+        "Category":Category,
+        "Mrp":Mrp,
+        "Upc":Upc
+    };
+
+    writeData.push(inventory);
+    obj=JSON.stringify(writeData);
+    fs.writeFileSync("inventory.json",obj);
+
+    input=prompt("IF you want enter data, Press 1 Then  Exist Press 0:");
+}
+while ( input ==  1  ) {
+   
+    let output=fs.readFileSync('inventory.json');
+
+    let data=JSON.parse(output);
+
+    console.log(data);
+}
+
+
+
+
+
