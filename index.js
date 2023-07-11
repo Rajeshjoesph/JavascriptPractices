d58f600ef0e5b9e4f5cee3c76d2598c8814576a6
“rajeshjoshph” <rajeshkannan1999.rk@gmail.com>
2023-07-11 08:40:08 -0700

    Added:Pratice Export module function added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const prompt=require('prompt-sync')();
 const { log } = require('console');
 const fs=require('fs');
 writeData=require("./inventory");
+var inventory_file= require("./inventory_file");
+
 
 console.log("1 - Inventory entiers:");
 console.log("2 - Display inventory:");
@@ -9,36 +11,7 @@ console.log("3 - Search by Category:");
 let process=parseFloat( prompt("Enter the Operation as Press " ));
 switch (process) {
     case 1:
-        function inventory() {
-            let Pcode=prompt("Enter PCode:");
-            let Desc=prompt("Enter Descripation:");
-            let Category=prompt("Enter Category:");
-            let Mrp=prompt("Enter MRP:");
-            let Upc=prompt("Enter Upc:");
-        
-            var inventory={
-                "Pcode":Pcode,
-                "Desc":Desc,
-                "Category":Category,
-                "Mrp":Mrp,
-                "Upc":Upc
-            };
-        
-            writeData.push(inventory);
-            obj=JSON.stringify(writeData);
-            fs.writeFileSync("inventory.json",obj);
-        
-            input=parseInt( prompt("IF you want enter data, Press 1 ,Then  Exist Press 0:"));
-    
-        }
-        inventory()
-            
-            if (input == 0) {
-                console.log("Inventory data is Added");
-                return false;
-            }else{
-                inventory()
-            }
+        inventory_file.inventoryRegister();
         break;
     case 2:
         let output=fs.readFileSync('inventory.json');
